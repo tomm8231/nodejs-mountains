a4f8c0bb37de26dfb93158ae6961adeeab14c5f3
Tommyk <tomm8231@stud.kea.dk>
2023-08-30 12:06:24 +0200

    Optimised code for get requests

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require("express")
+
 const app = express()
 
-const mountains = ["Mount Kilimanjaro", "Denali", "Mount Logan", "Aconcagua", "Mount Everest"]
+const mountains = [
+    { id: 1, name: "Kangchenjunga", height: 8586 },
+    { id: 2, name: "K2", height: 8611 },
+    { id: 3, name: "Lhotse", height: 8516 },
+    { id: 4, name: "Manaslu", height: 8163 },
+    { id: 5, name: "Makalu", height: 8485 },
+    { id: 6, name: "Dhaulagiri", height: 8167 },
+    { id: 7, name: "Annapurna", height: 8091 },
+    { id: 8, name: "Nanga Parbat", height: 8126 },
+    { id: 9, name: "Cho Oyu", height: 8188 },
+    { id: 10, name: "Mount Everest", height: 8848 }
+]
 
 app.get("/mountains", (req, res) => {
-    res.send({mountains})
+    const allMountains = mountains.map(mountain => ({
+        id: mountain.id,
+        name: mountain.name,
+        height: mountain.height
+    }))
+    
+    res.send(allMountains)
 })
 
 app.get("/mountains/:id", (req, res) => {
-    const id = req.params.id - 1
-    const mountain = mountains[id]
-    res.send({mountain})
+    const {id} = req.params
+    const mountainById = mountains[id - 1]
+
+    res.send(mountainById)
 })
 
 app.listen(8080)
